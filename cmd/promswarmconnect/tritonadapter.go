b3fef0488737a50942cee6c5e840ab2670d202eb
Joonas Loppi <joonas@joonas.fi>
2019-01-26 15:59:44 +0200

    move contents of tritonstructs.go to tritonadapter.go

--- cmd/promswarmconnect/tritonadapter.go
+++ cmd/promswarmconnect/tritonadapter.go
@@ -1,5 +1,18 @@
 package main
 
+// cannot reference from Prometheus sources because this was inlined
+type TritonDiscoveryResponseContainer struct {
+	ServerUUID  string `json:"server_uuid"`
+	VMAlias     string `json:"vm_alias"`
+	VMBrand     string `json:"vm_brand"`
+	VMImageUUID string `json:"vm_image_uuid"`
+	VMUUID      string `json:"vm_uuid"`
+}
+
+type TritonDiscoveryResponse struct {
+	Containers []TritonDiscoveryResponseContainer `json:"containers"`
+}
+
 func serviceInstancesToTritonContainers(services []Service) TritonDiscoveryResponse {
 	containers := []TritonDiscoveryResponseContainer{}
 
